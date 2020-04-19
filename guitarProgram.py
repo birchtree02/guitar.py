bf38083c6eaa406f6e72a413a537c7486c27920a
unknown <birchtree02>
2020-04-19 16:43:24 +0100

    added setString method in GuitarDisplay class to set what finger goes on that string and where

--- guitarProgram.py
+++ guitarProgram.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from colours import *
 
 # sizes
@@ -6,6 +7,7 @@ WIDTH = 250
 HEIGHT = 250
 BUFFER = 10
 FONT_SIZE = 30
+CIRCLE_FONT_SIZE = 15
 
 # other values
 STRINGS = 6
@@ -25,17 +27,25 @@ class GuitarDisplay():
         self.initialiseValues()
         self.drawGrid()
 
+        self.setString(string=0, fret=2, number=2) # test code to display a G chord
+        self.setString(string=1, fret=1, number=1)
+        self.setString(string=2, fret=0, number=0)
+        self.setString(string=3, fret=0, number=0)
+        self.setString(string=4, fret=0, number=0)
+        self.setString(string=5, fret=2, number=3)
+
+
         pygame.display.update()
 
         self.waitForClose()
 
     def initialiseValues(self):    
-        self.stringsStartBufferY = BUFFER+FONT_SIZE*2 # to allow for string labels
+        self.stringsStartBufferY = BUFFER+FONT_SIZE*2 # to make space for string labels
         self.stringSize = HEIGHT-(self.stringsStartBufferY + BUFFER)
     
     def drawGrid(self):
         for i in range(STRINGS): # vertical
-            stringX = BUFFER + (self.stringSize//(STRINGS-1))*(i)
+            stringX = BUFFER + (self.stringSize//(STRINGS-1))*i
 
             self.drawText(self.guitar.getStringNotes()[i], (stringX-FONT_SIZE//4, BUFFER))
             pygame.draw.line(self.surface, LINE_COLOUR, (stringX, 0+self.stringsStartBufferY), (stringX, self.stringsStartBufferY+self.stringSize))
@@ -44,9 +54,16 @@ class GuitarDisplay():
             lineY = self.stringsStartBufferY + (self.stringSize//(FRETS))*i
             pygame.draw.line(self.surface, LINE_COLOUR, (0+BUFFER, lineY), (0+BUFFER+self.stringSize, lineY))
 
+    def setString(self, string, fret, number):
+        number = {-1:"x",0:"o",1:"\u2776",2:"\u2777",3:"\u2778"}[number]
+        labelX = BUFFER + (self.stringSize//(STRINGS-1))*string - CIRCLE_FONT_SIZE//2
+        labelY = self.stringsStartBufferY + fret*self.stringSize//FRETS - CIRCLE_FONT_SIZE//2 - (self.stringSize//FRETS)//2
+        self.drawText(str(number), (labelX, labelY))
+
+
     def drawText(self, text, position):
-        text_surface = pygame.font.Font(None, FONT_SIZE)
-        text = text_surface.render(text, True, TEXT_COLOUR)
+        font = pygame.font.Font(pygame.font.match_font("calibri"), CIRCLE_FONT_SIZE) if ord(text) > 128 else pygame.font.Font(None, FONT_SIZE)
+        text = font.render(text, True, TEXT_COLOUR, BG_COLOUR)
         self.surface.blit(text, position)
 
     def waitForClose(self):
